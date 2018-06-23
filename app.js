c2046860e2c79053d1fded9eef27ccffcd28612b
Tangry <tangrytangrytangry@hotmail.com>
2018-06-23 11:22:11 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// custom 404 page
+app.use(function (req, res, next) {
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not Found');
+});
+
+// custom 500 page
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.type('text/plain');
+    res.status(500);
+    res.send('500 - Server Error');
+    // res.status(500).send('Something broke!');
+});
+
+module.exports = app;
---
+++ bin/server.js
@@ -0,0 +1,122 @@
+#!/usr/bin/env node
+
+/**
+ * Module dependencies.
+ */
+
+var app = require('../app');
+var debug = require('debug')('site:server');
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+/**
+ * Read server configuration data.
+ */
+
+var configFileName = 'config.json';
+var configFileDir = path.join(__dirname, '..');
+var configFileFull;
+var configData;
+
+try {
+  configFileFull = path.join(configFileDir, configFileName);
+  configData = JSON.parse(fs.readFileSync(configFileFull))
+}
+catch (exception) {
+  console.log('Error reading configuration file ' + configFileFull + ':\n' + exception);
+  process.exit(1);
+}
+
+app.set('port', configData.server.port.value);
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+var port = normalizePort(process.env.PORT || app.get('port'));
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+console.log(configData.server.desc.value + ' started listening on port ' + port);
+console.log('date          = ' + new Date());
+console.log('description   = ' + configData.server.desc.value);
+console.log('server type   = ' + configData.server.type.value);
+console.log('environment   = ' + app.get('env'));
+console.log('version       = ' + process.version);
+console.log('directory     = ' + __dirname);
+console.log('file          = ' + __filename);
+console.log('configuration = ' + configFileFull);
+console.log('process       = ' + process.argv);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
