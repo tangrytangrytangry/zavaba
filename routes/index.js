e07212f453b2c92882372a8fe27bac261c346ce6
Tangry <tangrytangrytangry@hotmail.com>
2018-07-19 19:04:30 +0300

    Install i18n

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,10 @@ var debug = require('debug')('server:index');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('../views/home', { user: req.user, main_currentLang : req.currentLang });
+  // res.render('../views/home', { user: req.user, main_currentLang : req.currentLang });
+  let main_currentLang = {};
+  main_currentLang.zz = 'Some';
+  res.render('../views/home', { user: req.user, main_currentLang: main_currentLang });
 
 });
 
