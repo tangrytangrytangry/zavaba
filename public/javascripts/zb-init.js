be54064b7624fe5c31a53ed02b8b8fb8b28d3ee8
Tangry <tangrytangrytangry@hotmail.com>
2018-08-12 14:27:18 +0300

    Forgot password modal page

--- public/javascripts/zb-init.js
+++ public/javascripts/zb-init.js
@@ -8,7 +8,40 @@ jQuery(function () {
     zbInitNav();
 
     // Init login controller
-	var lc = new zbLoginController();
+    var lc = new zbLoginController();
+
+    // Init form validator
+    var rv = new zbResetValidator();
+
+    // Login retrieval form via email //
+    var ev = new zbEmailValidator();
+
+    $('#get-credentials-form').ajaxForm({
+        url: '/lost-password',
+        beforeSubmit: function (formData, jqForm, options) {
+            if (ev.validateEmail($('#email-tf').val())) {
+                ev.hideEmailAlert();
+                return true;
+            } else {
+                ev.showEmailAlert("<b>Error!</b> Please enter a valid email address");
+                return false;
+            }
+        },
+        success: function (responseText, status, xhr, $form) {
+            $('#cancel').html('OK');
+            $('#retrieve-password-submit').hide();
+            ev.showEmailSuccess("Check your email on how to reset your password.");
+        },
+        error: function (e) {
+            if (e.responseText == 'email-not-found') {
+                ev.showEmailAlert("Email not found. Are you sure you entered it correctly?");
+            } else {
+                $('#cancel').html('OK');
+                $('#retrieve-password-submit').hide();
+                ev.showEmailAlert("Sorry. There was a problem, please try again later.");
+            }
+        }
+    });
 
     return;
 
