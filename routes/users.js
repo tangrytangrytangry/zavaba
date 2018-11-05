294a8e6073dc16db158b6e77a4c49e51d8b49ec0
Tangry <tangrytangrytangry@hotmail.com>
2018-11-05 14:23:54 +0300

    Add Activity promise

--- routes/users.js
+++ routes/users.js
@@ -11,30 +11,8 @@ router.get('/', function (req, res, next) {
   var today = new Date();
   var curDate = today.getFullYear() * 10000 + (today.getMonth() + 1) * 100 + today.getDate();
 
-  function findMaxItem(date) {
-
-    let searchDate = 0;
-    let maxItem = 0;
-
-    if (date) {
-      searchDate = date;
-    } else {
-      let today = new Date();
-      searchDate = today.getFullYear() * 10000 + (today.getMonth() + 1) * 100 + today.getDate();
-    }
-
-    Activity.find({ date: searchDate }, 'item', function (err, activity) {
-      if (err) return handleError(err);
-      for (let index = 0; index < activity.length; index++) {
-        if (activity[index]['item'] > maxItem) { maxItem = activity[index]['item'] ; }
-      }
-    })
-
-    return maxItem;
-
-  }
-
-  let newItem = findMaxItem() + 1;
+  let newItem = findMaxItem();
+  newItem = newItem + 1;
 
   var newActivity = new Activity(
     {
@@ -44,14 +22,54 @@ router.get('/', function (req, res, next) {
         kind: "ordinary"
       }
     });
-  console.log(newActivity);
+  // console.log(newActivity);
 
   newActivity.save(function (err, newActivity) {
     if (err) return console.error(err);
   });
 
-
   res.send('newActivity = ' + newActivity);
 });
+
+
+function findMaxItem(date) {
+
+  let searchDate = 0;
+  let maxItem = 0;
+
+  if (date) {
+    searchDate = date;
+  } else {
+    let today = new Date();
+    searchDate = today.getFullYear() * 10000 + (today.getMonth() + 1) * 100 + today.getDate();
+  }
+
+  /*
+  const query = Activity.find(); // `query` is an instance of `Query`
+  query.setOptions({ item: 1 });
+  query.collection(Activity.collection);
+  query.where('item').eq(1).exec(function (err, activities) {
+    console.log(activities);
+    if (err) return handleError(err);
+    for (let index = 0; index < activities.length; index++) {
+      if (activities[index]['item'] > maxItem) { maxItem = activities[index]['item']; }
+    }
+  });
+  */
+
+  let promiseCount = Activity.countDocuments({ date: searchDate }).exec();;
+
+  maxItem = cbCount();
+
+  async function cbCount() {
+  //  if (err) return handleError(err);
+    const dateCount = await promiseCount;
+    return dateCount;
+  }
+
+  return maxItem;
+
+}
+
 
 module.exports = router;
